361cd23b6c969ddf2e4e8a908120ece3e60809f9
shine911 <deathbook1999@gmail.com>
2014-04-26 17:19:39 +0700

    Update recovery_keys.c

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -14,7 +14,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_HOMEPAGE || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -30,7 +30,7 @@ int device_handle_key(int key_code, int visible) {
             case KEY_UP:
             case KEY_VOLUMEUP:
                 return HIGHLIGHT_UP;
-            case KEY_HOMEPAGE:
+            case KEY_HOME:
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
@@ -41,15 +41,16 @@ int device_handle_key(int key_code, int visible) {
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
-                if (!get_allow_toggle_display())
+                if (!get_allow_toggle_display()) {
                     return GO_BACK;
+                }
                 break;
             case KEY_LEFTBRACE:
             case KEY_ENTER:
             case BTN_MOUSE:
             case KEY_CAMERA:
             case KEY_F21:
-            case KEY_SEND:            
+            case KEY_SEND:
             case KEY_END:
             case KEY_BACKSPACE:
             case KEY_SEARCH:
